4f520c28855a916b9a3ac2f90d295a753f500e2e
James Herring <herring_jr@mercer.edu>
2026-03-31 12:44:01 -0400

    Initial commit.

---
+++ include/MyLibrary.h
@@ -0,0 +1,11 @@
+#ifndef MYLIBRARY_H
+#define MYLIBRARY_H
+
+class MyLibrary {
+public:
+    static int sum(int a, int b);
+    static int subtract(int a, int b);
+    static int mult(int a, int b);
+};
+
+#endif // MYLIBRARY_H
---
+++ src/MyLibrary.cpp
@@ -0,0 +1,15 @@
+#include "MyLibrary.h"
+
+#include <climits>
+
+int MyLibrary::sum(int a, int b) {
+    return 2*a;
+}
+
+int MyLibrary::subtract(int a, int b) {
+    return 42;
+}
+
+int MyLibrary::mult(int a, int b) {
+    return INT_MAX;
+}
